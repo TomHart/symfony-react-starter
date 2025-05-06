a932c0d0cef3f12d024ed35064c160ffc8e6db40
Tom Hart <thomas.hart@sky.uk>
2025-05-06 09:26:15 +0100

    Redirect if logged in

--- assets/pages/Landing/Register.tsx
+++ assets/pages/Landing/Register.tsx
@@ -2,8 +2,37 @@ import React from 'react';
 import {ArrowLeft} from 'lucide-react';
 import {Link} from "react-router-dom";
 import {SymfonyFormWrapper} from "@/components/form/SymfonyForm";
+import {useUser} from "@/provider/UserContext";
 
 export default function Register() {
+    const {user, loading} = useUser();
+
+    if (loading) {
+        return <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className='m-auto animate-spin'
+        >
+            <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
+        </svg>;
+    }
+
+    if (user) {
+        window.location.href = '/admin';
+        return <p>Already logged in, redirecting...</p>;
+    }
+
+    return <RegisterInner/>;
+}
+
+function RegisterInner() {
 
     return (
         <div className="m-auto w-full max-w-md space-y-8">
@@ -12,7 +41,8 @@ export default function Register() {
                 <p className="text-muted-foreground">Enter your details to sign up</p>
             </div>
             <div className="space-y-6">
-                <SymfonyFormWrapper formUrl='/register/render' submitUrl='/register' onSuccess={() => window.location.href = '/admin'}/>
+                <SymfonyFormWrapper formUrl='/register/render' submitUrl='/register'
+                                    onSuccess={() => window.location.href = '/admin'}/>
             </div>
             <div className="text-center text-sm">
                 Have an account already?{' '}
